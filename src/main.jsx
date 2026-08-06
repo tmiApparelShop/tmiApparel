46e70f570701df03a5da2d2926a9008ff91498d0
tmiapparel <tmiapparels@gmail.com>
2026-08-06 11:51:43 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './index.css'; // Make sure this matches your actual CSS file name if you have one
 
 const rootElement = document.getElementById('root');
 
